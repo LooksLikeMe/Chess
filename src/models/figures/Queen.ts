8da7f1f409f71375ed25ee83dcd3fca96e5a341b
Mikhael <56895233+LooksLikeMe@users.noreply.github.com>
2022-07-18 21:48:16 +0200

    little fixes

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -22,6 +22,9 @@ export class Queen extends Figure {
     if (this.cell.isEmptyHorizontal(target)) {
       return true
     }
+    if (this.cell.isEmptyDiagonal(target)) {
+      return true
+    }
     return false
   }
 }
